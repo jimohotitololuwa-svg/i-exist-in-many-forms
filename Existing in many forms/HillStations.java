4d07116c1d390f3946268cb816be779e9b10367e
jimohotitololuwa-svg <jimohotitololuwa@gmail.com>
2026-06-06 17:52:54 +0100

    Add files via upload

---
+++ Existing in many forms/HillStations.java
@@ -0,0 +1,36 @@
+public class HillStations{
+      void location(){
+        System.out.println("Hill Stations are located in the mountainous regions.");
+      }
+      void famousfor(){
+        System.out.println("Famous for :");
+        System.out.println("1. Natural Beauty");
+        System.out.println("2. Tourism");
+        System.out.println("3. Adventure Sports");
+}
+}
+class Manali extends HillStations{
+      void location(){
+        System.out.println("Manali is located in the Himalayas.");
+      }
+
+      void famousfor(){
+        System.out.println("Famous for :");
+        System.out.println("1. Education Institutions");
+        System.out.println("2.Tourism");
+        System.out.println("3. technology");
+      }
+}
+
+class Gulmarg extends HillStations{
+        void location(){
+        System.out.println("Gulmarg is located in the Kashmir region.");
+        }
+        void famousfor(){
+        System.out.println("Famous for :");
+        System.out.println("1. Skiing");
+        System.out.println("2. Commercial Tourism");
+        System.out.println("3. innovation");
+        }
+}
+
---
+++ Existing in many forms/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        HillStations A = new HillStations();
+        HillStations G = new Gulmarg();
+        HillStations M = new Manali();
+
+        
+        A.location();
+        A.famousfor();
+        
+       
+        G.location();
+        G.famousfor();
+        
+        M.location();
+        M.famousfor();
+
+    }
+}
